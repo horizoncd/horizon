73c4a21179dcf7ae91c78662717bdc336923b44c
gejunqiang <gejunqiang@corp.netease.com>
2021-09-13 11:25:08 +0800

    add user unit test

---
+++ pkg/user/manager_test.go
@@ -0,0 +1,57 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+
+	"g.hz.netease.com/horizon/lib/orm"
+	"g.hz.netease.com/horizon/pkg/user/models"
+)
+
+var (
+	db *gorm.DB
+	ctx context.Context
+)
+
+func Test(t *testing.T) {
+	var (
+		name = "Tony"
+		email = "tony@163.com"
+		oidcID   = "H12323"
+		oidcType = "ne"
+	)
+	u, err := Mgr.Create(ctx, &models.User{
+		Name:     name,
+		Email:    email,
+		OIDCId:   oidcID,
+		OIDCType: oidcType,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, err := json.Marshal(u)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(string(b))
+
+	u2, err := Mgr.GetByOIDCMeta(ctx, oidcID, oidcType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, name, u2.Name)
+	assert.Equal(t, email, u2.Email)
+}
+
+func TestMain(m *testing.M) {
+	db, _ = orm.NewSqliteDB("")
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		panic(err)
+	}
+	ctx = orm.NewContext(nil, db)
+	m.Run()
+}
