f53809e6b240d33203b25d47bad67830dd3a4e9c
gejunqiang <gejunqiang@corp.netease.com>
2021-09-14 21:03:49 +0800

    change comment to english

--- db/migrations/20210908_initial_schema.up.sql
+++ db/migrations/20210908_initial_schema.up.sql
@@ -15,16 +15,16 @@ CREATE TABLE `group` (
 
 -- user table
 CREATE TABLE `user` (
-    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `name` varchar(64) NOT NULL DEFAULT '' COMMENT '名称',
-    `email` varchar(64) NOT NULL DEFAULT '' COMMENT '邮件地址',
-    `phone` varchar(32) DEFAULT NULL COMMENT '电话号码',
-    `oidc_id` varchar(64) NOT NULL COMMENT 'oidc系统ID，比如oidc_type为netease的话，此字段为工号',
-    `oidc_type` varchar(64) NOT NULL COMMENT 'oidc类型，当前主要为netease',
-    `admin`  tinyint(1) NOT NULL COMMENT '是否是管理员，0-否，1-是',
-    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted_at` datetime DEFAULT NULL COMMENT '删除时间，未删除时为null',
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(64) NOT NULL DEFAULT '',
+    `email` varchar(64) NOT NULL DEFAULT '',
+    `phone` varchar(32) DEFAULT NULL COMMENT,
+    `oidc_id` varchar(64) NOT NULL COMMENT 'oidc id, which is a unique key in oidc system.',
+    `oidc_type` varchar(64) NOT NULL COMMENT 'oidc type, such as google, github, gitlab etc.',
+    `admin`  tinyint(1) NOT NULL COMMENT 'is system admin，0-false，1-true',
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` datetime DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_deleted_at` (`deleted_at`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
