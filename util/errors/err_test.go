07dcf0933d39b425c5ff12ca49dfeeb6dfd16419
gejunqiang <gejunqiang@corp.netease.com>
2021-09-23 22:28:01 +0800

    fix unit test

--- util/errors/err_test.go
+++ util/errors/err_test.go
@@ -14,14 +14,14 @@ const ErrCodeDuplicate = ErrorCode("Duplicate")
 func TestError_WrapError(t *testing.T) {
 	err := E("A", errSome)
 	err = E("B", err)
-	assert.Equal(t, "B: A: hello world", err.Error())
+	assert.Equal(t, "B - A - hello world", err.Error())
 	assert.Equal(t, string(ErrCodeInternalError), Code(err))
 }
 
 func TestError_TranslateError(t *testing.T) {
 	err := E("A", ErrCodeInternalError, "fatal error")
 	err = E("B", 409, ErrCodeDuplicate, err, "duplicate")
-	assert.Equal(t, "B: A: <409 Duplicate - duplicate> <500 InternalError - fatal error> ", err.Error())
+	assert.Equal(t, "B - A - <409 Duplicate - duplicate> <500 InternalError - fatal error> ", err.Error())
 	assert.Equal(t, string(ErrCodeDuplicate), Code(err))
 	assert.Equal(t, "duplicate", Message(err))
 }
@@ -41,7 +41,7 @@ func TestError_Status(t *testing.T) {
 func TestError_ChangeCode(t *testing.T) {
 	err := E("A", ErrCodeInternalError, errSome)
 	err = E("B", 409, ErrCodeDuplicate, err)
-	assert.Equal(t, "B: A: <409 Duplicate - > <500 InternalError - > hello world", err.Error())
+	assert.Equal(t, "B - A - <409 Duplicate - > <500 InternalError - > hello world", err.Error())
 	assert.Equal(t, string(ErrCodeDuplicate), Code(err))
 	assert.Equal(t, "hello world", Message(err))
 }
