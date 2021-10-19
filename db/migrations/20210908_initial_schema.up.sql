a8b340118553da51801147ad00826d281117f976
gejunqiang <gejunqiang@corp.netease.com>
2021-10-19 10:37:10 +0800

    update db script

--- db/migrations/20210908_initial_schema.up.sql
+++ db/migrations/20210908_initial_schema.up.sql
@@ -100,6 +100,27 @@ CREATE TABLE `template_release`
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4;
 
+-- member table
+CREATE TABLE `member`
+(
+    `id`            int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `resource_type` varchar(64)      NOT NULL COMMENT 'group\application\cluster',
+    `resource_id`   int(11) unsigned NOT NULL COMMENT 'resource id',
+    `role`          varchar(64)      NOT NULL COMMENT 'binding role name',
+    `member_type`   tinyint(1) COMMENT '0-USER, 1-group',
+    `membername_id` int(11) unsigned NOT NULL COMMENT 'UserID or GroupID',
+    `granted_by`      int(11) unsigned NOT NULL COMMENT 'who grant the role',
+    `created_by`    int(11) unsigned NOT NULL COMMENT 'who create the role',
+    `created_at`    datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`    datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at`    datetime                  DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `idx_deleted_at` (`deleted_at`),
+    UNIQUE KEY `idx_resource_member` (`resource_type`, `resource_id`, `member_type`, `membername_id`, `deleted_at`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4;
+
 -- application table
 CREATE TABLE `application`
 (
@@ -246,22 +267,6 @@ CREATE TABLE `pipelinerun`
     `created_by`       int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'creator',
     PRIMARY KEY (`id`),
     KEY `idx_cluster_action` (`cluster`, `action`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
-  DEFAULT CHARSET = utf8mb4;
-
--- cluster history table
-CREATE TABLE `cluster_history`
-(
-    `id`               int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `cluster`          varchar(64)      NOT NULL COMMENT 'cluster name',
-    `action`           varchar(64)      NOT NULL COMMENT 'action',
-    `pipelinerun_id`   int(11) unsigned NULL COMMENT 'pipelinerun_id if related to a pipelinerun',
-    `description`      varchar(2048)    NULL COMMENT 'the history description',
-    `created_at`       datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `created_by`       int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'creator',
-    PRIMARY KEY (`id`),
-    KEY `idx_action` (`cluster`, `action`)
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4;
