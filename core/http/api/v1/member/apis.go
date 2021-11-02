e2e0f770719583529621f5a972e0eacb876ea2a7
tom <hzsunjianliang@corp.netease.com>
2021-11-02 14:39:16 +0800

    fix

--- core/http/api/v1/member/apis.go
+++ core/http/api/v1/member/apis.go
@@ -49,14 +49,14 @@ func (a *API) CreateGroupMember(c *gin.Context) {
 		return
 	}
 
+	postMember.ResourceType = membermodels.TypeGroupStr
+	postMember.ResourceID = uint(uintID)
+
 	if err := a.validatePostMember(c, membermodels.TypeGroup, postMember); err != nil {
 		response.AbortWithRequestError(c, common.InvalidRequestParam,
 			err.Error())
 		return
 	}
-
-	postMember.ResourceType = membermodels.TypeGroupStr
-	postMember.ResourceID = uint(uintID)
 
 	retMember, err := a.memberCtrl.CreateMember(c, postMember)
 	if err != nil {
@@ -83,14 +83,14 @@ func (a *API) CreateApplicationMember(c *gin.Context) {
 		return
 	}
 
+	postMember.ResourceType = membermodels.TypeApplicationStr
+	postMember.ResourceID = uint(uintID)
+
 	if err := a.validatePostMember(c, membermodels.TypeApplication, postMember); err != nil {
 		response.AbortWithRequestError(c, common.InvalidRequestParam,
 			err.Error())
 		return
 	}
-
-	postMember.ResourceType = membermodels.TypeApplicationStr
-	postMember.ResourceID = uint(uintID)
 
 	retMember, err := a.memberCtrl.CreateMember(c, postMember)
 	if err != nil {
@@ -204,12 +204,6 @@ func (a *API) validRole(ctx context.Context, role string) error {
 // validatePostMember validate postMember body according to resourceType
 func (a *API) validatePostMember(ctx context.Context, resourceType membermodels.ResourceType,
 	postMember *member.PostMember) error {
-	if membermodels.ResourceType(postMember.ResourceType) != resourceType {
-		return fmt.Errorf("resourceType not match")
-	}
-	if err := validResourceType(postMember.ResourceType); err != nil {
-		return err
-	}
 
 	if err := validMemberType(postMember.MemberType); err != nil {
 		return err
@@ -219,15 +213,6 @@ func (a *API) validatePostMember(ctx context.Context, resourceType membermodels.
 		return err
 	}
 
-	return nil
-}
-
-func validResourceType(resourceType string) error {
-	switch membermodels.ResourceType(resourceType) {
-	case membermodels.TypeGroup, membermodels.TypeApplication, membermodels.TypeApplicationCluster:
-	default:
-		return fmt.Errorf("invalid resourceType")
-	}
 	return nil
 }
 
