33f62bfa3f9229f17eed68004ddf56c9970d3b49
gejunqiang <gejunqiang@corp.netease.com>
2022-01-11 10:34:20 +0800

    fix default step replicas

--- pkg/cluster/cd/cd.go
+++ pkg/cluster/cd/cd.go
@@ -390,7 +390,7 @@ func (c *cd) GetClusterState(ctx context.Context,
 				clusterState.Step = &Step{
 					Index:    1,
 					Total:    1,
-					Replicas: []int{len(clusterPodMap)},
+					Replicas: []int{1},
 				}
 				return clusterState, nil
 			}
