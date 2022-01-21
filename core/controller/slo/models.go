ea4f6e4a264c605b66a9a61c8e6636a87781c7a8
yeka <wurongjun@corp.netease.com>
2022-01-21 14:34:32 +0800

    Pipeline SLO

--- core/controller/slo/models.go
+++ core/controller/slo/models.go
@@ -1,10 +1,12 @@
 package slo
 
 type PipelineSLO struct {
-	Name                string  // 英文名，build、deploy等
-	DisplayName         string  // 中文名，构建、发布等
-	Count               int     // 次数
-	RequestAvailability float64 // 请求可用率 99.9945
-	RTAvailability      float64 // RT可用率 99.9925
-	RT                  uint    // RT临界值 60 单位秒
+	Name                string  `json:"name"`                // 英文名，build、deploy等
+	DisplayName         string  `json:"displayName"`         // 中文名，构建、发布等
+	Count               int     `json:"count"`               // 次数
+	RequestAvailability float64 `json:"requestAvailability"` // 请求可用率 99.9945
+	RequestSlo          float64 `json:"requestSlo"`          // 请求可用率SLO 99.99
+	RTAvailability      float64 `json:"rtAvailability"`      // RT可用率 99.9825
+	RTSlo               float64 `json:"rtSlo"`               // RT可用率SLO 99.98
+	RT                  uint    `json:"rt"`                  // RT临界值 60 单位秒
 }
