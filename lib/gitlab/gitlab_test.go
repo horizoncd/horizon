002d0237e2c87a36fd1fae51cc8b32cf27c6bff9
yeka <wurongjun@corp.netease.com>
2022-02-23 20:06:07 +0800

    Revert "modify the way of handling gitlab error"
    This reverts commit 17cb40e8217cf26af8178cf37db2a31061ce9bf6.

--- lib/gitlab/gitlab_test.go
+++ lib/gitlab/gitlab_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"testing"
 
+	"g.hz.netease.com/horizon/pkg/util/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/xanzy/go-gitlab"
 )
@@ -105,6 +107,7 @@ func Test(t *testing.T) {
 	// 1. get group first. will return 404 error
 	_, err = g.GetGroup(ctx, groupPath)
 	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusNotFound, errors.Status(err))
 
 	// 2. create this group
 	group, err = g.CreateGroup(ctx, groupName, groupName, intToPtr(rootGroupID))
@@ -174,6 +177,7 @@ func Test(t *testing.T) {
 	// 10. get this branch again, will return 404 error
 	_, err = g.GetBranch(ctx, pid, newBranch)
 	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusNotFound, errors.Status(err))
 
 	// 11. write files to new branch
 	projectBytes, err := json.MarshalIndent(project, "", "    ")
