08fd9aa4e885b2d8a1ad1bae55a98efd888fe6b3
yeka <wurongjun@corp.netease.com>
2022-11-18 14:18:12 +0800

    Fix v2 not use cmdb

--- pkg/hook/handler/cmdb_handler.go
+++ pkg/hook/handler/cmdb_handler.go
@@ -37,39 +37,51 @@ func (h *CMDBEventHandler) Process(event *hook.EventCtx) error {
 }
 
 func (h *CMDBEventHandler) ProcessCreateApplication(event *hook.EventCtx) error {
+	currentUser, err := common.UserFromContext(event.Ctx)
+	if err != nil {
+		return errors.New("can not get user from context")
+	}
+	var (
+		priority string
+		name     string
+	)
+
 	e := event.Event
 	switch info := e.(type) {
 	case *application.GetApplicationResponse:
-		currentUser, err := common.UserFromContext(event.Ctx)
-		if err != nil {
-			return errors.New("can not get user from context")
-		}
-		accounts := make([]cmdb.Account, 0)
-		accounts = append(accounts, cmdb.Account{
-			Account:     currentUser.GetName(),
-			AccountType: cmdb.User,
-		})
-
-		var priority cmdb.PriorityType
-		switch applicationmodel.Priority(info.Priority) {
-		case applicationmodel.P0:
-			priority = cmdb.P0
-		case applicationmodel.P1:
-			priority = cmdb.P1
-		case applicationmodel.P2:
-			priority = cmdb.P2
-		default:
-			priority = cmdb.P2
-		}
-		req := cmdb.CreateApplicationRequest{
-			Name:     info.Name,
-			Priority: priority,
-			Admin:    accounts,
-		}
-		return h.ctl.CreateApplication(event.Ctx, req)
+		priority = info.Priority
+		name = info.Name
+	case *application.CreateApplicationResponseV2:
+		priority = info.Priority
+		name = info.Name
 	default:
-		return fmt.Errorf("type error,need GetApplicationResponse, get %s", reflect.TypeOf(e).Name())
+		return fmt.Errorf("type error, need GetApplicationResponse | "+
+			"CreateApplicationResponseV2, get %s", reflect.TypeOf(e).Name())
 	}
+
+	accounts := make([]cmdb.Account, 0)
+	accounts = append(accounts, cmdb.Account{
+		Account:     currentUser.GetName(),
+		AccountType: cmdb.User,
+	})
+
+	var cmdbPriority cmdb.PriorityType
+	switch applicationmodel.Priority(priority) {
+	case applicationmodel.P0:
+		cmdbPriority = cmdb.P0
+	case applicationmodel.P1:
+		cmdbPriority = cmdb.P1
+	case applicationmodel.P2:
+		cmdbPriority = cmdb.P2
+	default:
+		cmdbPriority = cmdb.P2
+	}
+	req := cmdb.CreateApplicationRequest{
+		Name:     name,
+		Priority: cmdbPriority,
+		Admin:    accounts,
+	}
+	return h.ctl.CreateApplication(event.Ctx, req)
 }
 
 func (h *CMDBEventHandler) ProcessDeleteApplication(event *hook.EventCtx) error {
@@ -83,35 +95,49 @@ func (h *CMDBEventHandler) ProcessDeleteApplication(event *hook.EventCtx) error 
 }
 
 func (h *CMDBEventHandler) ProcessCreateCluster(event *hook.EventCtx) error {
+	currentUser, err := common.UserFromContext(event.Ctx)
+	if err != nil {
+		return errors.New("can not get user from context")
+	}
+	var (
+		environment     string
+		name            string
+		applicationName string
+	)
+
 	e := event.Event
 	switch info := e.(type) {
 	case *cluster.GetClusterResponse:
-		currentUser, err := common.UserFromContext(event.Ctx)
-		if err != nil {
-			return errors.New("can not get user from context")
-		}
-		accounts := make([]cmdb.Account, 0)
-		accounts = append(accounts, cmdb.Account{
-			Account:     currentUser.GetName(),
-			AccountType: cmdb.User,
-		})
-
-		env, err := cmdb.ToCmdbEnv(info.Scope.Environment)
-		if err != nil {
-			return err
-		}
-		req := cmdb.CreateClusterRequest{
-			Name:                info.Name,
-			ApplicationName:     info.Application.Name,
-			Env:                 env,
-			ClusterServerStatus: cmdb.StatusReady,
-			AutoAddDocker:       cmdb.AutoAddContainer,
-			Admin:               accounts,
-		}
-		return h.ctl.CreateCluster(event.Ctx, req)
+		environment = info.Scope.Environment
+		name = info.Name
+		applicationName = info.Application.Name
+	case *cluster.CreateClusterResponseV2:
+		environment = info.Scope.Environment
+		name = info.Name
+		applicationName = info.Application.Name
 	default:
-		return fmt.Errorf("type error,need string, get %s", reflect.TypeOf(e).Name())
+		return fmt.Errorf("type error, need GetClusterResponse | CreateClusterResponseV2, get %s", reflect.TypeOf(e).Name())
 	}
+
+	accounts := make([]cmdb.Account, 0)
+	accounts = append(accounts, cmdb.Account{
+		Account:     currentUser.GetName(),
+		AccountType: cmdb.User,
+	})
+
+	env, err := cmdb.ToCmdbEnv(environment)
+	if err != nil {
+		return err
+	}
+	req := cmdb.CreateClusterRequest{
+		Name:                name,
+		ApplicationName:     applicationName,
+		Env:                 env,
+		ClusterServerStatus: cmdb.StatusReady,
+		AutoAddDocker:       cmdb.AutoAddContainer,
+		Admin:               accounts,
+	}
+	return h.ctl.CreateCluster(event.Ctx, req)
 }
 
 func (h *CMDBEventHandler) ProcessDeleteCluster(event *hook.EventCtx) error {
