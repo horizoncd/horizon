959aa9f6519687c17ac28a2b7c9b1c5aaa26a9d8
xu.zhu <xu.zhu.work@outlook.com>
2023-04-12 19:51:23 +0800

    fix: add ut for pkg/cluster/tekton/collector
    Signed-off-by: xu.zhu <xu.zhu.work@outlook.com>

---
+++ pkg/cluster/tekton/collector/collector_dummy_test.go
@@ -0,0 +1,59 @@
+package collector
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	prmodels "github.com/horizoncd/horizon/pkg/pipelinerun/models"
+	"github.com/horizoncd/horizon/pkg/server/global"
+	"github.com/stretchr/testify/assert"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+
+	tektonmock "github.com/horizoncd/horizon/mock/pkg/cluster/tekton"
+)
+
+func TestDummyCollector(t *testing.T) {
+	var pr *v1beta1.PipelineRun
+	if err := json.Unmarshal([]byte(pipelineRunJSON), &pr); err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	ctl := gomock.NewController(t)
+	tek := tektonmock.NewMockInterface(ctl)
+	tek.EXPECT().GetPipelineRunLogByID(ctx, gomock.Any()).Return(getPipelineRunLog(pr))
+	tek.EXPECT().GetPipelineRunByID(ctx, gomock.Any()).Return(pr, nil)
+
+	c := NewDummyCollector(tek)
+
+	businessData := &global.HorizonMetaData{
+		Application: "app",
+		Cluster:     "cluster",
+		Environment: "test",
+	}
+
+	// collect
+	collectResult, err := c.Collect(ctx, pr, businessData)
+	assert.Nil(t, err)
+	b, _ := json.Marshal(collectResult)
+	t.Logf("%v", string(b))
+
+	// 1. getLatestPipelineRunLog
+	prModel := &prmodels.Pipelinerun{
+		CIEventID: "cttzw",
+	}
+	_, err = c.GetPipelineRunLog(ctx, prModel)
+	assert.Nil(t, err)
+
+	// 2. getLatestPipelineRunObject
+	obj, err := c.GetPipelineRunObject(ctx, collectResult.PrObject)
+	assert.Nil(t, err)
+	assert.Nil(t, obj)
+
+	// 3. getLatestPipelineRun
+	_, err = c.GetPipelineRun(ctx, prModel)
+	assert.Nil(t, err)
+}
--- pkg/cluster/tekton/collector/collector_test.go
+++ pkg/cluster/tekton/collector/collector_test.go
@@ -12,126 +12,127 @@ import (
 )
 
 //nolint:lll
-func Test_resolveObjMetadata(t *testing.T) {
-	jsonBody1 := `{
-        "metadata":{
-            "name":"test-music-docker-q58rp",
-            "namespace":"tekton-resources",
-            "creationTimestamp": "2021-07-16T08:51:54Z",
-            "labels":{
-                "app.kubernetes.io/managed-by":"Helm",
-                "tekton.dev/pipeline":"default",
-                "triggers.tekton.dev/eventlistener":"default-listener",
-                "triggers.tekton.dev/trigger":"",
-                "triggers.tekton.dev/triggers-eventid":"cttzw"
+const pipelineRunJSON = `{
+    "metadata":{
+        "name":"test-music-docker-q58rp",
+        "namespace":"tekton-resources",
+        "creationTimestamp": "2021-07-16T08:51:54Z",
+        "labels":{
+            "app.kubernetes.io/managed-by":"Helm",
+            "tekton.dev/pipeline":"default",
+            "triggers.tekton.dev/eventlistener":"default-listener",
+            "triggers.tekton.dev/trigger":"",
+            "triggers.tekton.dev/triggers-eventid":"cttzw"
+        }
+    },
+    "status":{
+        "conditions":[
+            {
+                "type":"Succeeded",
+                "status":"True",
+                "lastTransitionTime":"2021-06-24T06:38:18Z",
+                "reason":"Succeeded",
+                "message":"Tasks Completed: 2 (Failed: 0, Cancelled 0), Skipped: 0"
             }
-        },
-        "status":{
-            "conditions":[
-                {
-                    "type":"Succeeded",
-                    "status":"True",
-                    "lastTransitionTime":"2021-06-24T06:38:18Z",
-                    "reason":"Succeeded",
-                    "message":"Tasks Completed: 2 (Failed: 0, Cancelled 0), Skipped: 0"
+        ],
+        "startTime":"2021-06-24T06:36:11Z",
+        "completionTime":"2021-06-24T06:38:18Z",
+        "taskRuns":{
+            "test-music-docker-q58rp-build-g8khd":{
+                "pipelineTaskName":"build",
+                "status":{
+                    "conditions":[
+                        {
+                            "type":"Succeeded",
+                            "status":"True",
+                            "lastTransitionTime":"2021-06-24T06:36:43Z",
+                            "reason":"Succeeded",
+                            "message":"All Steps have completed executing"
+                        }
+                    ],
+                    "podName":"test-music-docker-q58rp-build-g8khd-pod-mwsld",
+                    "startTime":"2021-06-24T06:36:11Z",
+                    "completionTime":"2021-06-24T06:36:43Z",
+                    "steps":[
+                        {
+                            "terminated":{
+                                "exitCode":0,
+                                "reason":"Completed",
+                                "startedAt":"2021-06-24T06:36:18Z",
+                                "finishedAt":"2021-06-24T06:36:26Z",
+                                "containerID":"docker://3cccbd086c26e83e41fe8fcd86ef4e0f42e3963371c581e458df223b94da8d1e"
+                            },
+                            "name":"git",
+                            "container":"step-git",
+                            "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/tekton-builder@sha256:14194e518981f5d893b85e170a28ba8aa80c2c610f63cfba814b6a460f48dc29"
+                        },
+                        {
+                            "terminated":{
+                                "exitCode":0,
+                                "reason":"Completed",
+                                "startedAt":"2021-06-24T06:36:26Z",
+                                "finishedAt":"2021-06-24T06:36:34Z",
+                                "containerID":"docker://58d06c0a4bfa8212620e5a85a42e9af0768a4adb9ade2219dc75aee4d650ff23"
+                            },
+                            "name":"compile",
+                            "container":"step-compile",
+                            "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/tekton-builder@sha256:14194e518981f5d893b85e170a28ba8aa80c2c610f63cfba814b6a460f48dc29"
+                        },
+                        {
+                            "terminated":{
+                                "exitCode":0,
+                                "reason":"Completed",
+                                "message":"[{\"key\":\"properties\",\"value\":\"harbor.cloudnative.com/app-demo-test:app-demo-b1f57848-20210624143634 ssh://git@cloudnative.com:22222/app/app-demo.git helloworld b1f578488e3123e97ec00b671db143fb8f0abecf\",\"type\":\"TaskRunResult\"}]",
+                                "startedAt":"2021-06-24T06:36:34Z",
+                                "finishedAt":"2021-06-24T06:36:42Z",
+                                "containerID":"docker://9189624ad3981fd738ec5bf286f1fc5b688d71128b9827820ebc2541b2801dae"
+                            },
+                            "name":"image",
+                            "container":"step-image",
+                            "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/kaniko-executor@sha256:473d6dfb011c69f32192e668d86a47c0235791e7e857c870ad70c5e86ec07e8c"
+                        }
+                    ]
                 }
-            ],
-            "startTime":"2021-06-24T06:36:11Z",
-            "completionTime":"2021-06-24T06:38:18Z",
-            "taskRuns":{
-                "test-music-docker-q58rp-build-g8khd":{
-                    "pipelineTaskName":"build",
-                    "status":{
-                        "conditions":[
-                            {
-                                "type":"Succeeded",
-                                "status":"True",
-                                "lastTransitionTime":"2021-06-24T06:36:43Z",
-                                "reason":"Succeeded",
-                                "message":"All Steps have completed executing"
-                            }
-                        ],
-                        "podName":"test-music-docker-q58rp-build-g8khd-pod-mwsld",
-                        "startTime":"2021-06-24T06:36:11Z",
-                        "completionTime":"2021-06-24T06:36:43Z",
-                        "steps":[
-                            {
-                                "terminated":{
-                                    "exitCode":0,
-                                    "reason":"Completed",
-                                    "startedAt":"2021-06-24T06:36:18Z",
-                                    "finishedAt":"2021-06-24T06:36:26Z",
-                                    "containerID":"docker://3cccbd086c26e83e41fe8fcd86ef4e0f42e3963371c581e458df223b94da8d1e"
-                                },
-                                "name":"git",
-                                "container":"step-git",
-                                "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/tekton-builder@sha256:14194e518981f5d893b85e170a28ba8aa80c2c610f63cfba814b6a460f48dc29"
+            },
+            "test-music-docker-q58rp-deploy-xzjkg":{
+                "pipelineTaskName":"deploy",
+                "status":{
+                    "conditions":[
+                        {
+                            "type":"Succeeded",
+                            "status":"True",
+                            "lastTransitionTime":"2021-06-24T06:38:18Z",
+                            "reason":"Succeeded",
+                            "message":"All Steps have completed executing"
+                        }
+                    ],
+                    "podName":"test-music-docker-q58rp-deploy-xzjkg-pod-zkcc4",
+                    "startTime":"2021-06-24T06:36:43Z",
+                    "completionTime":"2021-06-24T06:38:18Z",
+                    "steps":[
+                        {
+                            "terminated":{
+                                "exitCode":0,
+                                "reason":"Completed",
+                                "startedAt":"2021-06-24T06:36:48Z",
+                                "finishedAt":"2021-06-24T06:38:18Z",
+                                "containerID":"docker://fb2579fe83579e1918b5dcedc35f3686cad8ac632cc750d6d92f556341b5f7bb"
                             },
-                            {
-                                "terminated":{
-                                    "exitCode":0,
-                                    "reason":"Completed",
-                                    "startedAt":"2021-06-24T06:36:26Z",
-                                    "finishedAt":"2021-06-24T06:36:34Z",
-                                    "containerID":"docker://58d06c0a4bfa8212620e5a85a42e9af0768a4adb9ade2219dc75aee4d650ff23"
-                                },
-                                "name":"compile",
-                                "container":"step-compile",
-                                "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/tekton-builder@sha256:14194e518981f5d893b85e170a28ba8aa80c2c610f63cfba814b6a460f48dc29"
-                            },
-                            {
-                                "terminated":{
-                                    "exitCode":0,
-                                    "reason":"Completed",
-                                    "message":"[{\"key\":\"properties\",\"value\":\"harbor.cloudnative.com/app-demo-test:app-demo-b1f57848-20210624143634 ssh://git@cloudnative.com:22222/app/app-demo.git helloworld b1f578488e3123e97ec00b671db143fb8f0abecf\",\"type\":\"TaskRunResult\"}]",
-                                    "startedAt":"2021-06-24T06:36:34Z",
-                                    "finishedAt":"2021-06-24T06:36:42Z",
-                                    "containerID":"docker://9189624ad3981fd738ec5bf286f1fc5b688d71128b9827820ebc2541b2801dae"
-                                },
-                                "name":"image",
-                                "container":"step-image",
-                                "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/kaniko-executor@sha256:473d6dfb011c69f32192e668d86a47c0235791e7e857c870ad70c5e86ec07e8c"
-                            }
-                        ]
-                    }
-                },
-                "test-music-docker-q58rp-deploy-xzjkg":{
-                    "pipelineTaskName":"deploy",
-                    "status":{
-                        "conditions":[
-                            {
-                                "type":"Succeeded",
-                                "status":"True",
-                                "lastTransitionTime":"2021-06-24T06:38:18Z",
-                                "reason":"Succeeded",
-                                "message":"All Steps have completed executing"
-                            }
-                        ],
-                        "podName":"test-music-docker-q58rp-deploy-xzjkg-pod-zkcc4",
-                        "startTime":"2021-06-24T06:36:43Z",
-                        "completionTime":"2021-06-24T06:38:18Z",
-                        "steps":[
-                            {
-                                "terminated":{
-                                    "exitCode":0,
-                                    "reason":"Completed",
-                                    "startedAt":"2021-06-24T06:36:48Z",
-                                    "finishedAt":"2021-06-24T06:38:18Z",
-                                    "containerID":"docker://fb2579fe83579e1918b5dcedc35f3686cad8ac632cc750d6d92f556341b5f7bb"
-                                },
-                                "name":"deploy",
-                                "container":"step-deploy",
-                                "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/tekton-builder@sha256:14194e518981f5d893b85e170a28ba8aa80c2c610f63cfba814b6a460f48dc29"
-                            }
-                        ]
-                    }
+                            "name":"deploy",
+                            "container":"step-deploy",
+                            "imageID":"docker-pullable://harbor.cloudnative.com/cloudnative/library/tekton-builder@sha256:14194e518981f5d893b85e170a28ba8aa80c2c610f63cfba814b6a460f48dc29"
+                        }
+                    ]
                 }
             }
         }
     }
-	`
+}
+`
+
+func Test_resolveObjMetadata(t *testing.T) {
 	var pr *v1beta1.PipelineRun
-	if err := json.Unmarshal([]byte(jsonBody1), &pr); err != nil {
+	if err := json.Unmarshal([]byte(pipelineRunJSON), &pr); err != nil {
 		t.Fatal(err)
 	}
 	type args struct {
