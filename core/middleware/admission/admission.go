b781db6253303909d668a1fe96b30ba9f33fc194
Kiloson <4closetool3@gmail.com>
2024-05-07 20:55:49 +0800

    fix: some bugs
    Signed-off-by: Kiloson <4closetool3@gmail.com>

--- core/middleware/admission/admission.go
+++ core/middleware/admission/admission.go
@@ -58,49 +58,46 @@ func Middleware(skippers ...middleware.Skipper) gin.HandlerFunc {
 				return
 			}
 		}
-		if object != nil {
-			// fill in the request url query into admission request options
-			queries := c.Request.URL.Query()
-			options := make(map[string]interface{}, len(queries))
-			for k, v := range queries {
-				if len(v) == 1 {
-					options[k] = v[0]
-				} else {
-					options[k] = v
-				}
+		// fill in the request url query into admission request options
+		queries := c.Request.URL.Query()
+		options := make(map[string]interface{}, len(queries))
+		for k, v := range queries {
+			if len(v) == 1 {
+				options[k] = v[0]
+			} else {
+				options[k] = v
 			}
-			admissionRequest := &admissionwebhook.Request{
-				Operation:   admissionmodels.Operation(attr.GetVerb()),
-				Resource:    attr.GetResource(),
-				Name:        attr.GetName(),
-				SubResource: attr.GetSubResource(),
-				Version:     attr.GetAPIVersion(),
-				Object:      object,
-				Options:     options,
-			}
-			admissionRequest, err = admissionwebhook.Mutating(c, admissionRequest)
-			if err != nil {
-				response.AbortWithRPCError(c,
-					rpcerror.ParamError.WithErrMsg(fmt.Sprintf("admission mutating failed: %v", err)))
-				return
-			}
-			if err := admissionwebhook.Validating(c, admissionRequest); err != nil {
-				response.AbortWithRPCError(c,
-					rpcerror.ParamError.WithErrMsg(fmt.Sprintf("admission validating failed: %v", err)))
-				return
-			}
-			newBodyBytes, err := json.Marshal(admissionRequest.Object)
+		}
+		admissionRequest := &admissionwebhook.Request{
+			Operation:   admissionmodels.Operation(attr.GetVerb()),
+			Resource:    attr.GetResource(),
+			Name:        attr.GetName(),
+			SubResource: attr.GetSubResource(),
+			Version:     attr.GetAPIVersion(),
+			Object:      object,
+			Options:     options,
+		}
+		admissionRequest, err = admissionwebhook.Mutating(c, admissionRequest)
+		if err != nil {
+			response.AbortWithRPCError(c,
+				rpcerror.ParamError.WithErrMsg(fmt.Sprintf("admission mutating failed: %v", err)))
+			return
+		}
+		if err := admissionwebhook.Validating(c, admissionRequest); err != nil {
+			response.AbortWithRPCError(c,
+				rpcerror.ParamError.WithErrMsg(fmt.Sprintf("admission validating failed: %v", err)))
+			return
+		}
+		if admissionRequest.Object != nil {
+			bodyBytes, err = json.Marshal(admissionRequest.Object)
 			if err != nil {
 				response.AbortWithRPCError(c,
 					rpcerror.ParamError.WithErrMsg(fmt.Sprintf("marshal request body failed, err: %v", err)))
 				return
 			}
-			// restore the request body
-			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(newBodyBytes))
-			c.Next()
-		} else {
-			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
-			c.Next()
 		}
+		// restore the request body
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		c.Next()
 	}, skippers...)
 }
